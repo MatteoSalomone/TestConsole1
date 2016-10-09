ed5448052856133d298891516bea4d0bcc3be86d
Matteo Salomone <Matteo Salomone>
2016-10-09 17:48:44 +0200

    Primo commit: classi parziali

---
+++ src/ConsoleTest1/Dog1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleTest1
+{
+    partial class Dog
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            private set {
+                //look at value
+                _name = value;
+            }
+        }
+        public Dog(string name)
+        {
+            _name = name;
+        }
+        public void Speak(int times, string what="bark")
+        {
+            for (int i = 0; i < times; i++)
+            {
+                Console.WriteLine(what);
+            }
+        }
+    }
+}
---
+++ src/ConsoleTest1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleTest1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Dog muttley = new Dog("Muttley");
+
+            muttley.Speak(2);
+            
+            muttley.Sleep();
+        }
+    }
+}
